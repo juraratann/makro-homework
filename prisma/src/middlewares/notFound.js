d06967b57e6db36080f766b6b45d1ae8f62c1e17
juraratann <annyanny@gmail.com>
2024-01-29 17:19:36 +0700

    init

--- prisma/src/middlewares/notFound.js
+++
@@ -1,5 +0,0 @@
-const notFoundHandler = (req, res, next) => {
-  res.status(404).json({ message: "Not Found" });
-};
-
-module.exports = notFoundHandler;
--- prisma/src/utils/cloudUpload.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("../config/cloudinary");
-
-const cloudUpload = async (path) => {
-  const res = await cloudinary.uploader.upload(path);
-
-  return res.secure_url;
-};
-
-module.exports = cloudUpload;
